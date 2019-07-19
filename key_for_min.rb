76b4dd8b9b5e032bc16e6f58184315acab8e1bc1
kellyeryan <kellyeryan@gmail.com>
2019-07-19 14:19:11 -0400

    Finish key for min value lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require "pry"
 
 def key_for_min_value(name_hash)
+  min_name = nil
+  min_value = "Not set yet"
 
+  name_hash.each do |name, value|
+    if min_value == "Not set yet"
+      min_value = value
+      min_name = name
+    end
+
+    if min_value > value
+      min_value = value
+      min_name = name
+    end
+  end
+  min_name
 end
